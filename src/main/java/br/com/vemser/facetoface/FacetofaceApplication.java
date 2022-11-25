f6774c3c23c6df87211e8c990a0b3732a70f4115
Julio Rocha <juliogabrielmr@gmail.com>
2022-11-25 16:47:47 -0300

    Criando API-FaceToFace

---
+++ src/main/java/br/com/vemser/facetoface/FacetofaceApplication.java
@@ -0,0 +1,13 @@
+package br.com.vemser.facetoface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacetofaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacetofaceApplication.class, args);
+	}
+
+}
