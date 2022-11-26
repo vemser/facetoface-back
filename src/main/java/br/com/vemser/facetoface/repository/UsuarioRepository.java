9ae4826c7ca4a375c82bf31a5ed1024f53d4c5be
Matheus Goncalves <math3usgoncalves@protonmail.com>
2022-11-26 17:37:03 -0300

    Iniciando UsuarioService

--- src/main/java/br/com/vemser/facetoface/repository/UsuarioRepository.java
+++ src/main/java/br/com/vemser/facetoface/repository/UsuarioRepository.java
@@ -4,6 +4,9 @@ import br.com.vemser.facetoface.entity.UsuarioEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<UsuarioEntity,Integer> {
+    Optional<UsuarioEntity> findByEmail(String email);
 }
--- src/main/java/br/com/vemser/facetoface/service/UsuarioService.java
+++ src/main/java/br/com/vemser/facetoface/service/UsuarioService.java
@@ -1,12 +1,39 @@
 package br.com.vemser.facetoface.service;
 
+import br.com.vemser.facetoface.dto.usuario.UsuarioDTO;
+import br.com.vemser.facetoface.entity.UsuarioEntity;
 import br.com.vemser.facetoface.repository.UsuarioRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UsuarioService {
     private final UsuarioRepository usuarioRepository;
+    private final ObjectMapper objectMapper;
 
+    public UsuarioEntity listarUsuarioEntityPeloId(Integer idUsuario) throws Exception {
+        return usuarioRepository.findById(idUsuario)
+                .orElseThrow(() -> new Exception("Usuário com id " + idUsuario + " não foi encontrado."));
+    }
+
+    public Optional<UsuarioEntity> listarOptionalUsuarioPorEmail(String email) {
+        return usuarioRepository.findByEmail(email);
+    }
+
+    public Integer getIdUsuarioLogado() {
+        return Integer.parseInt(String.valueOf(SecurityContextHolder.getContext().getAuthentication().getPrincipal()));
+    }
+
+    public UsuarioDTO listarUsuarioLogado() throws Exception {
+        Integer idUsuarioLogado = getIdUsuarioLogado();
+        UsuarioEntity usuario = listarUsuarioEntityPeloId(idUsuarioLogado);
+
+        return objectMapper.convertValue(usuario, UsuarioDTO.class);
+    }
+    
 }
