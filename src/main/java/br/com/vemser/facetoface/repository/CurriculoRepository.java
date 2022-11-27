4f743fa8a4c0c7e7c97b4e04537cddaa9c50a990
Julio Rocha <juliogabrielmr@gmail.com>
2022-11-27 15:18:00 -0300

    curriculo

--- src/main/java/br/com/vemser/facetoface/repository/CurriculoRepository.java
+++ src/main/java/br/com/vemser/facetoface/repository/CurriculoRepository.java
@@ -1,7 +1,10 @@
 package br.com.vemser.facetoface.repository;
 
+import br.com.vemser.facetoface.entity.CandidatoEntity;
 import br.com.vemser.facetoface.entity.CurriculoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CurriculoRepository extends JpaRepository<CurriculoEntity, Integer> {
+
+    CurriculoEntity findByCandidato(CandidatoEntity candidatoEntity);
 }
--- src/main/java/br/com/vemser/facetoface/service/CurriculoService.java
+++ src/main/java/br/com/vemser/facetoface/service/CurriculoService.java
@@ -1,5 +1,7 @@
 package br.com.vemser.facetoface.service;
 
+import br.com.vemser.facetoface.dto.candidato.CandidatoDTO;
+import br.com.vemser.facetoface.entity.CandidatoEntity;
 import br.com.vemser.facetoface.entity.CurriculoEntity;
 import br.com.vemser.facetoface.entity.ImageEntity;
 import br.com.vemser.facetoface.exceptions.RegraDeNegocioException;
@@ -7,6 +9,11 @@ import br.com.vemser.facetoface.repository.CurriculoRepository;
 import br.com.vemser.facetoface.repository.ImageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Base64Utils;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
@@ -14,8 +21,31 @@ public class CurriculoService {
 
     private final CurriculoRepository curriculoRepository;
 
+    private final CandidatoService candidatoService;
+
     public CurriculoEntity findById(Integer idPerfil) throws RegraDeNegocioException {
         return curriculoRepository.findById(idPerfil)
                 .orElseThrow(() -> new RegraDeNegocioException("Imagem não encontrada!"));
+    }
+
+    public CurriculoEntity arquivarCurriculo(MultipartFile file, String email) throws IOException, RegraDeNegocioException {
+        CandidatoDTO candidato = candidatoService.findByEmail(email);
+        CandidatoEntity candidatoEntity = new CandidatoEntity();
+        candidatoEntity = candidatoService.findById(candidato.getIdCandidato());
+        String nomeArquivo = StringUtils.cleanPath(file.getOriginalFilename());
+        CurriculoEntity curriculo = new CurriculoEntity();
+        curriculo.setNome(nomeArquivo);
+        curriculo.setTipo(file.getContentType());
+        curriculo.setDado(file.getBytes());
+        curriculo.setCandidato(candidatoEntity);
+        return curriculoRepository.save(curriculo);
+    }
+
+    public String pegarCurriculoCandidato(String email) throws RegraDeNegocioException{
+        CandidatoDTO candidato = candidatoService.findByEmail(email);
+        CandidatoEntity candidatoEntity = new CandidatoEntity();
+        candidatoEntity = candidatoService.findById(candidato.getIdCandidato());
+        CurriculoEntity curriculo = curriculoRepository.findByCandidato(candidatoEntity);
+        return Base64Utils.encodeToString(curriculo.getDado());
     }
 }
