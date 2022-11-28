5d913ce71c0d072c41fa1594245e98b0e3bbcae6
Matheus Goncalves <math3usgoncalves@protonmail.com>
2022-11-28 18:32:57 -0300

    Add: EmailService

---
+++ src/main/java/br/com/vemser/facetoface/service/EmailService.java
@@ -0,0 +1,67 @@
+//package br.com.vemser.facetoface.service;
+//
+//import freemarker.template.Template;
+//import freemarker.template.TemplateException;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.mail.javamail.JavaMailSender;
+//import org.springframework.mail.javamail.MimeMessageHelper;
+//import org.springframework.stereotype.Component;
+//import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
+//
+//import javax.mail.MessagingException;
+//import javax.mail.internet.MimeMessage;
+//import java.io.IOException;
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//@Component
+//@RequiredArgsConstructor
+//public class EmailService {
+//    public static final String ENVIO_SENHA_TEMPLATE_HTML = "envio-senha-template.html";
+//    public static final String RECUPERACAO_SENHA_TEMPLATE_HTML = "recuperacao-senha-template.html";
+//    private final freemarker.template.Configuration fmConfiguration;
+//    @Value("${spring.mail.username}")
+//    private String from;
+//    private final JavaMailSender emailSender;
+//
+//    @Value("${link.confirmacao.entrevista}")
+//    private final String link;
+//
+//    public void sendEmailConfimacaoEntrevista() {
+//
+//    }
+//
+//    public void sendEmailEnvioSenha(String email, String senha) {
+//        String subject = "Cadastro concluído com sucesso.";
+//        sendEmail(email, senha, ENVIO_SENHA_TEMPLATE_HTML, subject);
+//    }
+//
+//    public void sendEmailRecuperacaoSenha(String email, String token) {
+//        final String subject = "Recuperação de senha concluída com sucesso.";
+//        sendEmail(email, token, RECUPERACAO_SENHA_TEMPLATE_HTML, subject);
+//    }
+//
+//    public void sendEmail(String email, String info, String nomeTemplate, String assunto) {
+//        MimeMessage mimeMessage = emailSender.createMimeMessage();
+//
+//        try {
+//            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+//            mimeMessageHelper.setFrom(from);
+//            mimeMessageHelper.setTo(email);
+//            mimeMessageHelper.setSubject(assunto);
+//            mimeMessageHelper.setText(getContentFromTemplate(info, nomeTemplate), true);
+//            emailSender.send(mimeMessageHelper.getMimeMessage());
+//        } catch (MessagingException | IOException | TemplateException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    public String getContentFromTemplate(String info, String nomeTemplate) throws IOException, TemplateException {
+//        Map<String, Object> dados = new HashMap<>();
+//        dados.put("email", from);
+//        dados.put("texto1", info);
+//        Template template = fmConfiguration.getTemplate(nomeTemplate);
+//        return FreeMarkerTemplateUtils.processTemplateIntoString(template, dados);
+//    }
+//}
