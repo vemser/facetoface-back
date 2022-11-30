657e7fb92341caf2a2b18b41f5b640519c0c162f
Bruno Pelliciari <bruno.pelliciari@hotmail.com>
2022-11-29 21:46:52 -0300

    add:curriculoServiceTest inicio

---
+++ src/test/java/br/com/vemser/facetoface/service/CurriculoServiceTest.java
@@ -0,0 +1,102 @@
+package br.com.vemser.facetoface.service;
+
+import br.com.vemser.facetoface.entity.CandidatoEntity;
+import br.com.vemser.facetoface.entity.CurriculoEntity;
+import br.com.vemser.facetoface.exceptions.RegraDeNegocioException;
+import br.com.vemser.facetoface.repository.CandidatoRepository;
+import br.com.vemser.facetoface.repository.CurriculoRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.HexFormat;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CurriculoServiceTest {
+    @InjectMocks
+    private CurriculoService curriculoService;
+
+    @Mock
+    private CandidatoService candidatoService;
+
+    @Mock
+    private CurriculoRepository curriculoRepository;
+
+    @Mock
+    private CandidatoRepository candidatoRepository;
+
+
+//    @Test
+//    public void deveTestarArquivarCurriculoComSucesso() throws RegraDeNegocioException, IOException {
+//        CandidatoEntity candidatoEntity = new CandidatoEntity();
+//        candidatoEntity.setIdCandidato(2);
+//        candidatoEntity.setEmail("teste@gmail.com.br");
+//        byte[] bytes = HexFormat.of().parseHex("e04fd020ea3a6910a2d808002b30309d");
+//        MultipartFile curriculo = new MockMultipartFile("imagem", bytes);
+//        String nomeArquivo = StringUtils.cleanPath(curriculo.getOriginalFilename());
+//
+//
+//        when(candidatoService.findByEmailEntity(any())).thenReturn(candidatoEntity);
+//        when(curriculoRepository.findByCandidato(any())).thenReturn(Optional.of(getCurriculoEntity()));
+//        when(curriculoRepository.save(any())).thenReturn(getCurriculoEntity());
+//
+//        curriculoService.arquivarCurriculo(curriculo, candidatoEntity.getEmail());
+//
+//
+//        verify(curriculoRepository, times(1)).save(getCurriculoEntity());
+//
+//    }
+
+    @Test
+    public void devePegarCurriculoCandidatoComSucesso() throws RegraDeNegocioException{
+        CandidatoEntity candidatoEntity = new CandidatoEntity();
+        candidatoEntity.setIdCandidato(2);
+        candidatoEntity.setEmail("teste@gmail.com.br");
+
+        when(candidatoService.findByEmailEntity(any())).thenReturn(candidatoEntity);
+        when(curriculoRepository.findByCandidato(any())).thenReturn(Optional.of(getCurriculoEntity()));
+        String curriculoBase64 = curriculoService.pegarCurriculoCandidato(candidatoEntity.getEmail());
+
+        assertNotNull(curriculoBase64);
+    }
+
+    @Test
+    public void deveTestarFindByIdComSucesso() throws RegraDeNegocioException {
+        Integer id = 2;
+
+        when(curriculoRepository.findById(anyInt())).thenReturn(Optional.of(getCurriculoEntity()));
+        CurriculoEntity curriculo = curriculoService.findById(id);
+
+        assertNotNull(curriculo);
+        assertEquals(curriculo.getIdCurriculo(), id);
+    }
+
+    @Test(expected = RegraDeNegocioException.class)
+    public void findByIdComErro() throws RegraDeNegocioException {
+        Integer id = 2;
+        when(curriculoRepository.findById(anyInt())).thenReturn(Optional.empty());
+        curriculoService.findById(id);
+    }
+
+
+    private static CurriculoEntity getCurriculoEntity(){
+        CandidatoEntity candidatoEntity = new CandidatoEntity();
+        candidatoEntity.setIdCandidato(2);
+        candidatoEntity.setEmail("teste@gmail.com.br");
+        byte[] bytes = HexFormat.of().parseHex("e04fd020ea3a6910a2d808002b30309d");
+        return new CurriculoEntity(2,
+                "curriculo",
+                "png",
+                bytes,
+                candidatoEntity);
+    }
+}
