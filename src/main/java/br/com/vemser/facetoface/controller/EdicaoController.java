234f3882d1571ac454b5dc69ba6b9074befd26f3
Bruno Pelliciari <bruno.pelliciari@hotmail.com>
2022-12-01 17:12:39 -0300

    add:criarEdicao

--- src/main/java/br/com/vemser/facetoface/controller/EdicaoController.java
+++ src/main/java/br/com/vemser/facetoface/controller/EdicaoController.java
@@ -1,15 +1,21 @@
 package br.com.vemser.facetoface.controller;
 
 import br.com.vemser.facetoface.controller.documentationinterface.EdicaoControllerInterface;
+import br.com.vemser.facetoface.dto.edicao.EdicaoDTO;
+import br.com.vemser.facetoface.dto.entrevista.EntrevistaCreateDTO;
+import br.com.vemser.facetoface.dto.entrevista.EntrevistaDTO;
 import br.com.vemser.facetoface.entity.EdicaoEntity;
 import br.com.vemser.facetoface.exceptions.RegraDeNegocioException;
 import br.com.vemser.facetoface.service.EdicaoService;
+import freemarker.template.TemplateException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.mail.MessagingException;
+import javax.validation.Valid;
+import java.io.IOException;
 
 @RequiredArgsConstructor
 @RestController
@@ -21,5 +27,10 @@ public class EdicaoController implements EdicaoControllerInterface {
     public ResponseEntity<EdicaoEntity> deleteFisico(@PathVariable("idEdicao") Integer id) throws RegraDeNegocioException {
         edicaoService.deleteFisico(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/criar-edicao")
+    public ResponseEntity<EdicaoEntity> cadastrarEdicao(@Valid @RequestBody EdicaoDTO edicaoDTO)  {
+        return new ResponseEntity<>(edicaoService.create(edicaoDTO), HttpStatus.CREATED);
     }
 }
--- src/main/java/br/com/vemser/facetoface/controller/documentationinterface/EdicaoControllerInterface.java
+++ src/main/java/br/com/vemser/facetoface/controller/documentationinterface/EdicaoControllerInterface.java
@@ -1,6 +1,10 @@
 package br.com.vemser.facetoface.controller.documentationinterface;
 
+import br.com.vemser.facetoface.dto.candidato.CandidatoCreateDTO;
+import br.com.vemser.facetoface.dto.candidato.CandidatoDTO;
+import br.com.vemser.facetoface.dto.edicao.EdicaoDTO;
 import br.com.vemser.facetoface.entity.EdicaoEntity;
+import br.com.vemser.facetoface.entity.enums.Genero;
 import br.com.vemser.facetoface.exceptions.RegraDeNegocioException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -8,6 +12,10 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.validation.Valid;
 
 public interface EdicaoControllerInterface {
     @Operation(summary = "Deleta a edição no sistema", description = "Deleta a edição no sistema")
@@ -18,4 +26,13 @@ public interface EdicaoControllerInterface {
     })
     @DeleteMapping
     ResponseEntity<EdicaoEntity> deleteFisico(@PathVariable("idUsuario") Integer id) throws RegraDeNegocioException;
+
+    @Operation(summary = "Criar cadastro de edição", description = "Cria edição no Sistema")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Cadastro de edição realizado com sucesso!"),
+            @ApiResponse(responseCode = "400", description = "Erro na inserção de dados."),
+            @ApiResponse(responseCode = "403", description = "Foi gerada uma exceção.")
+    })
+    @PostMapping
+    ResponseEntity<EdicaoEntity> cadastrarEdicao(@Valid @RequestBody EdicaoDTO edicaoDTO);
 }
